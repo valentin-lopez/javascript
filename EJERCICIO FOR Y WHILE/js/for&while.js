47150c4f46617da9c2fdd87ceabab43c7194f051
Valentin <valentinlt4@gmail.com>
2024-07-16 21:12:23 -0300

    eCommerce entrega uno

---
+++ EJERCICIO FOR Y WHILE/js/for&while.js
@@ -0,0 +1,6 @@
+let conta = 0;
+while(conta < 10) {
+    console.log(conta);
+
+    conta = i++;
+}
---
+++ EJERCICIO RESTAURANTE/restaurante.js
@@ -0,0 +1,8 @@
+let totalComida = 2800 + 1300;
+let totalBebida = 900 + 650;
+
+let total = totalComida + totalBebida;
+
+console.log("El total de comida es: "+ totalComida);
+console.log("El total bebida es: " + totalBebida);
+console.log("El total de la cuenta es: " + total);
